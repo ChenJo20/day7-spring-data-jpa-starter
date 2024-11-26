63eb7aa746a44e7e435b1a54314a770744c09ef9
CHENJO20 <CHENJO20@oocl.com>
2024-11-26 15:45:59 +0800

    refactor, use employeeRepository instead of employeeInMemoryRepository in EmployeeService.findAll()

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.oocl.springbootemployee.repository;
 import com.oocl.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    List<Employee> getAll();
 }
